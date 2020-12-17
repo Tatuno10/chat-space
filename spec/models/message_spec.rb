86267deeeed5626d830b75c7c54b04b6ac772ce3
Tatuno10 <zell8130@outlook.jp>
2020-12-17 21:48:49 +0900

    Update message_spec.rb

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,10 +1,30 @@
 require 'rails_helper'
 describe Message do
   describe "is invalid without a messsage" do
-    
+    describe "If you have a message, you can save it" do
+      
+    end
+
+    describe 'If you have an image, you can save it' do
+      
+    end
+
+    describe 'If you have a message and an image, you can save it' do
+      
+    end
   end
 
   describe "is not invalid without a message" do
-    
+    describe 'Cannot be saved without a message or image' do
+      
+    end
+
+    describe 'Cannot save without group_id' do
+      
+    end
+
+    describe 'Cannot save without user_id' do
+      
+    end
   end
 end
