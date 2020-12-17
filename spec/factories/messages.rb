4d0f6526796bdbd0337d88389193a516611d75f7
Tatuno10 <zell8130@outlook.jp>
2020-12-17 17:36:30 +0900

    Update messages.rb

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   
   factory :message do
     content {Faker::Lorem.sentence}
-    image {File.open("#{Rails.root}/Users/yamashitatatsuya/Downloads/staff2.jpg")}
+    image {File.open("#{Rails.root}/Public/images/staff2.jpg")}
     user
     group
   end
