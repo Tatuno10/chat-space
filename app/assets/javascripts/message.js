8a7cfa57a8ae7c27d108571cdbcc052222363740
Tatuno10 <zell8130@outlook.jp>
2020-12-20 14:33:25 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,3 +1,15 @@
 $(function(){
-  
+  $(".Chat-main__message-form").on("submit",function(e){
+    e.preventDefault()
+    let formData = new FormData(this);
+    let url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: formData,
+      dataType: json,
+      processData: false,
+      contentType: false
+    })
+  })
 });
