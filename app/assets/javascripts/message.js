ed765e3f36fc0800b1931ff34918f1fc406b5ff8
Tatuno10 <zell8130@outlook.jp>
2020-12-20 15:42:30 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,44 @@
 $(function(){
+
+  function buildHTML(message){
+    if (message.image) {
+      let html = `<div class="Chat-main__message-box">
+                    <div class="Chat-main__message-info">
+                      <div "class=Chat-main__poster-name">
+                        ${message.user_name}
+                      </div>
+                      <div class="Chat-space__message-date">
+                        ${message.created_st}
+                      </div>
+                    </div>
+                    <div class="Chat-main__message-text">
+                      <p class="Chat-main__content">
+                        ${message.content}
+                      </p>
+                      <img class="Chat-main__image" src="${message.image}">
+                    </div>
+                  </div>`
+      return html;
+    } else {
+      let html = `<div class="Chat-main__message-box">
+                    <div class="Chat-main__message-info">
+                      <div "class=Chat-main__poster-name">
+                        ${message.user.name}
+                      </div>
+                      <div class="Chat-space__message-date">
+                        ${message.created_st}
+                      </div>
+                    </div>
+                    <div class="Chat-main__message-text">
+                      <p class="Chat-main__content">
+                        ${message.content}
+                      </p>
+                    </div>
+                  </div>`
+      return html;
+    };
+  }
+
   $(".Chat-main__message-form").on("submit",function(e){
     e.preventDefault()
     let formData = new FormData(this);
@@ -10,6 +50,11 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data) {
+      let html = buildHTML(data);
+      $('.Chat-main__message-list').append(html)
+      $('form')[0].reset();
     })
   })
 });
