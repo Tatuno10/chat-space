2310190edb371d1b114acfe684b798186c810e22
Tatuno10 <zell8130@outlook.jp>
2020-12-22 01:21:01 +0900

    Update users.js

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -2,12 +2,17 @@ $(function(){
 
   $("#UserSearch__field").on("keyup", function() {
     let input = $("#UserSearch__field").val();
-    console.log(input);
     $.ajax({
-      type: 'GET',
-      url: '/users/index',
-      datatype: 'json',
-      data: {keyword: input}
+      type: "GET",
+      url: "/users",
+      data: {　keyword: input　},
+      datatype: "json"
     })
+    .done(function(users) {
+      console.log("成功です");
+    })
+    .done(function(users) {
+      console.log("失敗しました");
+    });
   })
 });
