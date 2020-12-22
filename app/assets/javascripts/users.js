bcfa6c00d6d7d0411d33c28bd31c509b6670f15e
Tatuno10 <zell8130@outlook.jp>
2020-12-22 18:15:28 +0900

    Update users.js

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -19,6 +19,17 @@ $(function() {
     $("#UserSearchResult").append(html);
   }
 
+  function addMember(user_name, user_id) {
+    let html = `
+                <div class="ChatMember">
+                  <p class="ChatMember__name">${user_name}</p>
+                  <input name="group[user_ids][]" type="hidden" value=${user_id} />
+                  <div class="ChatMember__remove ChatMember__button">削除</div>
+                </div>
+                `;
+    $(".ChatMembers").append(html)
+  }
+
   $("#UserSearch__field").on("keyup", function() {
     let input = $("#UserSearch__field").val();
     $.ajax({
@@ -42,5 +53,15 @@ $(function() {
     .fail(function() {
       alert("通信エラーです。ユーザーが表示できません。");
     });
+  });
+
+  $("#UserSearchResult").on('click', ".ChatMember__add", function() {
+    const userName = $(this).attr("data-user-name")
+    const userId = $(this).attr("data-user-id")
+    $(this).parent().remove();
+    addMember(userName, userId);
+  });
+  $(".ChatMembers").on('click', ".ChatMember__remove", function() {
+    $(this).parent().remove();
   });
 });
