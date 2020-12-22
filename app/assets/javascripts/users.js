e466e8bed3b489b6b760abfac76d833dc6e3ca35
Tatuno10 <zell8130@outlook.jp>
2020-12-22 17:12:11 +0900

    Update users.js

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,5 +1,24 @@
 $(function(){
 
+  function addUser(user) {
+    let html = `
+                <div class="ChatMember clearfix">
+                  <p class="ChatMember__name">${user.name}</p>
+                  <div class="ChatMember__add ChatMember__button" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+                </div>
+                `;
+    $("#UserSearchResult").append(html)
+  }
+
+  function addNoUser() {
+    let html = `
+                <div class="ChatMember clearfix">
+                  <p class="ChatMember__name">ユーザーが見つかりません</p>
+                </div>
+                `;
+    $("#UserSearchResult").append(html)
+  }
+
   $("#UserSearch__field").on("keyup", function() {
     let input = $("#UserSearch__field").val();
     $.ajax({
@@ -9,10 +28,19 @@ $(function(){
       datatype: "json"
     })
     .done(function(users) {
-      console.log("成功です");
+      $("#UserSearchList").empty();
+      if (users.length !== 0) {
+        users.forEach(function(user) {
+          addUser(user);
+        });
+      } else if (input.length == 0) {
+        return false;
+      } else {
+        addNoUser();
+      }
     })
-    .done(function(users) {
-      console.log("失敗しました");
+    .fail(function() {
+      alert('通信エラーです。ユーザが表示できません。');
     });
-  })
+  });
 });
